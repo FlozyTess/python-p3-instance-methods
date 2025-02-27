e34ffec1382dfddd95d107573fecf1ac7efb7f79
FlozyTess <cecilrency23@gmail.com>
2025-02-27 22:26:35 +0300

    updated the class and instances

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,8 @@
 
 class Dog:
     # Class body goes here
-
-    #Instance method definition
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")
     pass
